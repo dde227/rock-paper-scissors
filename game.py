ea1dae28b230bae9e83947a40d8d8259404c97f5
dde227 <84485283+dde227@users.noreply.github.com>
2021-05-25 20:52:37 -0400

    Generate computer choice

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
@@ -13,5 +15,9 @@ if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "sciss
 else:    
     print("OOPS, INVALID INPUT. PLEASE TRY AGAIN.")
     exit()
+
+valid_options = ["rock","paper","scissor"]
+computer_choice = random.choice(valid_options)
+print("COMPUTER CHOICE: ",computer_choice)
 
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
